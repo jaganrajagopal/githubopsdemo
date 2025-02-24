c45ccb708afbc14ed72f31573d54ade711f47149
Jagan Rajagopal <jaganrajagopalme@gmail.com>
2025-02-24 21:26:30 +0530

    trigger pipeline

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2_instance" {
   #ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (Change as needed)
   instance_type = "t2.micro"  
   tags = {
-    Name = "Terraform-EC2"
+    Name = "Terraform-EC2 "
   }
 }
